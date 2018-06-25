7b912d3b24ae375dc479f5b5cead11740ee86b1d
balaprasanna <balaprasannav2009@gmail.com>
2018-06-25 14:10:25 +0800

    Day01

---
+++ day01/hello.py
@@ -0,0 +1,28 @@
+print ("hello world")
+
+print ("hello world")
+
+
+print ("hello world")
+
+
+if condition:
+    pass
+
+
+
+
+if condition:
+    pass
+else:
+    pass
+
+
+if condition:
+    pass
+elif condition:
+    pass
+elif condition:
+    pass
+else:
+    pass
